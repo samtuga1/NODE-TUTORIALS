da5729e5cbc2b4c2bd431cef04d89fb649f3ff12
Bentil Shadrack <bentilshadrack72@gmail.com>
2023-05-04 09:20:28 +0000

    Change MongoClient to mongoose👀

--- util/database.js
+++ util/database.js
@@ -1,21 +1,35 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
+const mongoose = require("mongoose");
 
 var _db;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect(
-    // "mongodb+srv://samuel:lGqeTf9qbUxdg8ww@cluster0.5y1nvtp.mongodb.net/?retryWrites=true&w=majority"
-    "mongodb+srv://samuel:lGqeTf9qbUxdg8ww@cluster0.5y1nvtp.mongodb.net/test"
-  )
-    .then((client) => {
-      _db = client.db("shop");
-      callback();
-    })
-    .catch((err) => {
-      console.log(err);
-      throw err;
-    });
+// const mongoConnect = (callback) => {
+//   MongoClient.connect(
+//     // "mongodb+srv://samuel:lGqeTf9qbUxdg8ww@cluster0.5y1nvtp.mongodb.net/?retryWrites=true&w=majority"
+//     "mongodb+srv://samuel:lGqeTf9qbUxdg8ww@cluster0.5y1nvtp.mongodb.net/test"
+//   )
+//     .then((client) => {
+//       _db = client.db("shop");
+//       callback();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       throw err;
+//     });
+// };
+
+const DBCONNECT = (callback) => {
+  try {
+    mongoose.connect("mongodb+srv://samuel:lGqeTf9qbUxdg8ww@cluster0.5y1nvtp.mongodb.net/?retryWrites=true&w=majority");
+    console.log(
+      `Database connected in 🚀`
+    );
+    callback();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error connecting to database");
+  }
 };
 
 const getDb = () => {
@@ -25,5 +39,5 @@ const getDb = () => {
   throw "No database found";
 };
 
-exports.mongoConnect = mongoConnect;
+exports.mongoConnect = DBCONNECT;
 exports.getDb = getDb;
